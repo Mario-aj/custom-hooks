e2a3fb8488d5994560d6fea40fe3f240d8c37c72
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-07-16 23:06:48 +0100

    rename useLocalStorage file

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,3 @@
 export { useRecognition } from './speechAPI';
-export { default as useLocalStorage } from './storageAPI/useLocalStorage';
+export { default as useLocalStorage } from './storageAPI';
 export { useBreakPoint } from './useBreakPoint';
