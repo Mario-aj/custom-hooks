ec4f84b382a31b97fc55782f170b32546e03fbcc
Mario-alfredo-jorge <mariojorge1997@mail.ru>
2021-08-04 22:58:27 +0100

    add retrun type

--- src/hooks/useBreakPoint.ts
+++ src/hooks/useBreakPoint.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-export const useBreakPoint = () => {
+export const useBreakPoint = (): { breakPoint: number } => {
   const [breakPoint, setBreakPoint] = useState(window.innerWidth);
 
   useEffect(() => {
